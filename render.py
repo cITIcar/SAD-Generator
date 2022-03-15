ca62ea14f0664714779b6c1d129b561c8dcbcc2f
Pau Dietz Romero <dietz.romero@physik.uni-siegen.de>
2022-03-15 15:32:58 +0100

    Correct PEP8 mistakes in render.py

--- render.py
+++ render.py
@@ -1,12 +1,27 @@
-import math
+"""Contains functions to implement perspective transforms."""
+
 import numpy as np
 from numpy import sin, cos
 import cv2
 
-import config
-
 
 class Renderer:
+    """
+    
+
+    Attributes
+    ----------
+    config
+    angle_x
+    angle_y
+    angle_z
+    position_x
+    position_y
+    position_z
+    render_objects
+    horizon_fraction
+    """
+
     def __init__(self, config):
         self.config = config
         self.angle_x = np.pi / 2 + np.radians(config["camera_angle"])
@@ -19,21 +34,50 @@ class Renderer:
 
         self.render_objects = []
         self.update_position((self.position_x, self.position_y), self.angle_z)
-        horizon_point = self.h_segmentation @ np.array([[0],[1],[0]])
-        self.horizon_fraction = (horizon_point[1] / horizon_point[2]) / self.config["output_size"][1]
-
+        horizon_point = self.h_segmentation @ np.array([[0], [1], [0]])
+        self.horizon_fraction = ((horizon_point[1] / horizon_point[2]) /
+                                 self.config["output_size"][1])
 
     def project_point(self, coord):
+        """
+
+
+        Parameters
+        ----------
+        coord : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        coord_2d : TYPE
+            DESCRIPTION.
+
+        """
         x, y, z = coord
         coord_3d_h = np.array([-x, -y, -z, 1])
         coord_3d_h = self.translation @ coord_3d_h
-        coord_2d_h = self.config["intrinsic_camera_matrix"] @ self.rotation @ coord_3d_h
+        coord_2d_h = (self.config["intrinsic_camera_matrix"] @
+                      self.rotation @ coord_3d_h)
         coord_2d = coord_2d_h[:2] / coord_2d_h[2]
 
         return coord_2d
 
-
     def update_position(self, position, angle):
+        """
+        
+
+        Parameters
+        ----------
+        position : TYPE
+            DESCRIPTION.
+        angle : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         for obj in self.render_objects:
             if obj.update_position_step:
                 position, angle = obj.update_position_step(
@@ -66,14 +110,36 @@ class Renderer:
             [0, 0, 1, self.position_z],
             [0, 0, 0, 1]])
 
-        self.h_camera = self.config["intrinsic_camera_matrix"] @ self.rotation @ self.translation @ self.config["project2d"]
-        self.h_segmentation = self.config["intrinsic_segmentation_matrix"] @ self.rotation @ self.translation @ self.config["project2d"]
+        self.h_camera = (self.config["intrinsic_camera_matrix"] @
+                         self.rotation @ self.translation @
+                         self.config["project2d"])
+        self.h_segmentation = (self.config["intrinsic_segmentation_matrix"] @
+                               self.rotation @ self.translation @
+                               self.config["project2d"])
 
+    def update_ground_plane(self, image_real, image_segmentation,
+                            render_objects):
+        """
+        
 
-    def update_ground_plane(self, image_real, image_segmentation, render_objects):
+        Parameters
+        ----------
+        image_real : TYPE
+            DESCRIPTION.
+        image_segmentation : TYPE
+            DESCRIPTION.
+        render_objects : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         self.image_real = image_real
         self.image_segmentation = image_segmentation
-        self.render_objects = sorted(render_objects, key=lambda obj: obj.ordering)
+        self.render_objects = sorted(render_objects,
+                                     key=lambda obj: obj.ordering)
 
         for obj in self.render_objects:
             if obj.pre_transform_step:
@@ -83,15 +149,35 @@ class Renderer:
 
         self.render_objects.sort(key=lambda obj: obj.ordering)
 
-
     def render_images(self):
+        """
+        
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        perspective_camera : TYPE
+            DESCRIPTION.
+        perspective_segmentation : TYPE
+            DESCRIPTION.
+
+        """
         width, height = self.config["output_size"]
         rescale = self.config["rescale"]
-        perspective_camera = cv2.warpPerspective(self.image_real, self.h_camera, (width * rescale, height * rescale), flags=cv2.INTER_NEAREST)
-        perspective_segmentation = cv2.warpPerspective(self.image_segmentation, self.h_segmentation, (width, height), flags=cv2.INTER_NEAREST)
+        perspective_camera = cv2.warpPerspective(
+            self.image_real, self.h_camera,
+            (width * rescale, height * rescale), flags=cv2.INTER_NEAREST)
+        perspective_segmentation = cv2.warpPerspective(
+            self.image_segmentation, self.h_segmentation,
+            (width, height), flags=cv2.INTER_NEAREST)
 
-        perspective_camera[:int(len(perspective_camera) * self.horizon_fraction)] = 0
-        perspective_segmentation[:int(len(perspective_segmentation) * self.horizon_fraction)] = 0
+        perspective_camera[:int(len(perspective_camera) *
+                                self.horizon_fraction)] = 0
+        perspective_segmentation[:int(len(perspective_segmentation) *
+                                      self.horizon_fraction)] = 0
 
         for obj in self.render_objects:
             if obj.post_transform_step:
@@ -100,13 +186,14 @@ class Renderer:
                     image_segment=perspective_segmentation,
                     point=(self.position_x, self.position_y),
                     angle=self.angle_z,
-                    global_angle=self.angle_z, # TODO: track global angle
+                    global_angle=self.angle_z,
                     bird_to_camera_nice=self.h_camera,
                     bird_to_camera_segment=self.h_segmentation,
                     renderer=self)
 
         if rescale != 1:
-            perspective_camera = cv2.resize(perspective_camera, (width, height), interpolation=cv2.INTER_AREA)
+            perspective_camera = cv2.resize(
+                perspective_camera, (width, height),
+                interpolation=cv2.INTER_AREA)
 
         return perspective_camera, perspective_segmentation
-
