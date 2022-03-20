1b0227364a21bab920ef355de544536f8e9ecd6f
Merlin Mengel <merlin.mengel@mni.thm.de>
2022-03-20 11:44:46 +0100

    added docstrings to config.py

--- config.py
+++ config.py
@@ -3,7 +3,26 @@ import math
 import json
 
 class Config:
+    """This class contains the configuration.
+
+    For convenience it also adds a few keys, that can be immediately created from the 
+    given configuration file.
+
+    Attributes
+    ----------
+    config : dict
+        The configuration file
+    """
+
     def __init__(self, config_file, debug=False):
+        """Creates a new instance.
+
+        Parameters
+        ----------
+        config_file : str
+            Path to a config file.
+        """
+
         self.config = json.load(open(config_file))
 
         self.config["intrinsic_camera_matrix"] = Config.create_intrinsic_from(
@@ -21,12 +40,32 @@ class Config:
         self.config["camera_height_px"] = self.config["camera_height"] * self.config["px_per_cm"]
         self.config["debug"] = debug
 
-
     def __getitem__(self, key):
+        """Imlpements Pythons index operation for ease of use.
+
+        Parameters
+        ----------
+        key : str
+            The name of the item.
+
+        Returns
+        -------
+            The item.        
+        """
         return self.config[key]
 
-
     def create_intrinsic_from(fov, output_size):
+        """Create the intrinsic camera matrix.
+
+        Parameters
+        ----------
+        fov : int
+            Field of view of the camera in degrees.
+
+        Returns
+        -------
+            The intrinsic camera matrix
+        """
         output_width, output_height = output_size
         focal_length = (output_width / 2) / math.tan((fov / 180 * np.pi) / 2)
 
@@ -35,11 +74,15 @@ class Config:
             [0, -focal_length, output_height / 2, 0],
             [0, 0, 1, 0]])
 
-
     def create_project2d():
+        """Create a matrix to project 3D homogenous coordinates back into 2D.
+
+        Returns
+        -------
+            The project 2D matrix.
+        """
         return np.float32([
             [-1, 0, 0],
             [0, -1, 0],
             [0, 0, 0],
             [0, 0, 1]])
-
